9f284b99b302a71324499ce878730705ee156f3a
kdekorte <kdekorte@localhost>
2012-05-26 13:42:40 +0000

    Fix a couple of keyboard shortcuts as they were in the wrong shift state, similar to gnome-mplayer fix

--- src/gmtk_media_player.c
+++ src/gmtk_media_player.c
@@ -452,7 +452,7 @@ static gboolean player_key_press_event_callback(GtkWidget * widget, GdkEventKey 
     if (event->is_modifier)
         return TRUE;
 
-    if ((event->state & GDK_SHIFT_MASK) == 0 && (event->state & GDK_CONTROL_MASK) == 0
+    if ((event->state & GDK_CONTROL_MASK) == 0
         && (event->state & GDK_MOD1_MASK) == 0) {
         switch (event->keyval) {
         case GDK_Right:
