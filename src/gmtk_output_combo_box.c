d3c9fe1e3572f3a62b220251b3ddedb75dcf1326
hansecke@gmail.com <hansecke@gmail.com>
2012-06-12 18:49:58 +0000

    GMLIB logging wave 3: replace printf with gm_log in all gmtk* files

--- src/gmtk_output_combo_box.c
+++ src/gmtk_output_combo_box.c
@@ -53,7 +53,7 @@ gint sort_iter_compare_func(GtkTreeModel * model, GtkTreeIter * a, GtkTreeIter *
         break;
 
     default:
-        printf("unimplemented sort routine\n");
+        gm_log(FALSE, G_LOG_LEVEL_MESSAGE, "unimplemented sort routine\n");
     }
 
     return ret;
@@ -85,7 +85,7 @@ void pa_sink_cb(pa_context * c, const pa_sink_info * i, int eol, gpointer data)
 
 void context_state_callback(pa_context * context, gpointer data)
 {
-    //printf("context state callback\n");
+    gm_log(FALSE, G_LOG_LEVEL_DEBUG, "context state callback\n");
     int i;
 
     switch (pa_context_get_state(context)) {
@@ -179,7 +179,7 @@ static void gmtk_output_combo_box_init(GmtkOutputComboBox * output)
     card = -1;
 
     while (snd_card_next(&card) >= 0) {
-        //printf("card = %i\n", card);
+        gm_log(FALSE, G_LOG_LEVEL_DEBUG, "card = %i\n", card);
         if (card < 0)
             break;
         if (name != NULL) {
@@ -188,7 +188,7 @@ static void gmtk_output_combo_box_init(GmtkOutputComboBox * output)
         }
         name = malloc(32);
         sprintf(name, "hw:%i", card);
-        //printf("name = %s\n",name);
+        gm_log(FALSE, G_LOG_LEVEL_DEBUG, "name = %s\n", name);
 
         if ((err = snd_ctl_open(&handle, name, 0)) < 0) {
             continue;
